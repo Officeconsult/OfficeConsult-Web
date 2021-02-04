1029277e14409d55b4b23a5db8879d9b54dec568
internoffice <61001730+internoffice@users.noreply.github.com>
2021-02-04 14:20:07 +0100

    asd

--- serverMain.py
+++ serverMain.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, session
+import json
+
+config = {}
+
+with open("config.json", "r") as file:
+    config = json.loads(file.read())
 
 app = Flask(__name__, template_folder="/content", static_folder="/content")
+app.secret_key = config.get("secretCookieKey")
+
 
 @app.route("/")
 def index():
+    print(session)
     return "hello world"
 
 if __name__ == "__main__":
