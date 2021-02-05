6dcf6950e5a545ef74fb7a88dddfe70e8380bf48
GUA1-app <60985935+GUA1-app@users.noreply.github.com>
2021-02-05 11:20:31 +0100

    added som forms and stuff ^^

--- serverMain.py
+++ serverMain.py
@@ -22,24 +22,28 @@ db = SQLAlchemy(app)
 ### - ^^ APPLICATION SETUP / CONFIGURATION ^^ - ###
 
 
+### - VV DEVELOPMENT DEF VV - ###
+# used for constructing a clean database in a testing enviroment.
+def constructDatabase():
+    db.drop_all()
+    db.create_all()
+### - ^^ DEVELOPMENT DEF ^^ - ###
+
+
 ### - VV DATABASE MODELS VV - ###
 # users and accounts
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
+    emailConfirmed = db.Column(db.Boolean, nullable=False)
     hashedPassword = db.Column(db.String(100), nullable=False)
     hashSalt = db.Column(db.String(100), nullable=False)
     products = db.Column(db.PickleType)
+    roles = db.Column(db.PickleType, nullable=False)
     # representation when printed
     def __repr__(self):
         return f"USER: (username={self.username}, email={self.email})"
-    # constructor for new users
-    def __init__(self, username, email, hashedPassword, hashSalt):
-        self.username = username
-        self.email = email
-        self.hashedPassword = hashedPassword
-        self.hashSalt = hashSalt
 ### - ^^ DATABASE MODELS ^^ - ###
 
 
@@ -47,17 +51,28 @@ class User(db.Model):
 # index returns homePage
 @app.route("/")
 def index():
+    method_was_get = request.method == "GET"
+    if method_was_get:
+        print("GOT IT")
     return render_template("homepage/HTML/index.html")
-# if logged in, render dealmaker, else redir -> index
-@app.route("/dealmaker")
+# if already logged in redir, else show registration form
+@app.route("/register", methods=["GET","POST"])
 def dealmakerIndex():
-    login = session.get('login', False)    
-    access = "dealMaker" in session.get('products', [])
-    if login and access:
-        print("logged in")
-    else:
+    # if Get::
+    if request.method == "GET":
+        print("method was get")
+        login = session.get('login', False)
+        if login:
+            print("already logged in")
+            return redirect("/")
+        # show registartion form if not logged in
         print("NOT logged in")
-    return redirect("/")
+        return render_template("homepage/HTML/register.html")
+    # if the method is post::
+    if request.method == "POST":
+        data = request.form
+        print(data)
+    return "TADA"
 ### - ^^ ROUTES ^^ - ###
 
 
