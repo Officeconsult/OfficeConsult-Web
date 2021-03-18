95581b996634485f39ba1c73242460c26f4f3397
internoffice <70441087+Nyan-Doggo@users.noreply.github.com>
2021-03-18 11:20:23 +0100

    testing some cookie-stuff

--- content/homepage/JS/main.js
+++
@@ -1,8 +0,0 @@
-/* let data = {element: "barium"};
-
-fetch("/post/data/here", {
-  method: "POST", 
-  body: JSON.stringify(data)
-}).then(res => {
-  console.log("Request complete! response:", res);
-}); */
---
+++ content/js/main.js
@@ -0,0 +1,9 @@
+$("#CRM-Key-set-btn").click(function () {
+    var key = $("#CRM-Key-input-field").val();
+    setCookie("crmkey", key, 10);
+  });
+  
+  $("#CRM-Key-check-btn").click(function () {
+    alert(getCookie("crm-key"));
+  });
+  
