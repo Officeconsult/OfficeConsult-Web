7a54091d02f7eb02b467587b0f22d6c3da4e9081
internoffice <70441087+Nyan-Doggo@users.noreply.github.com>
2021-03-18 11:42:31 +0100

    added option to delete cookies and stylized some buttons

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 $("#CRM-Key-set-btn").click(function () {
-    var key = $("#CRM-Key-input-field").val();
-    setCookie("crmkey", key, 10);
-  });
-  
-  $("#CRM-Key-check-btn").click(function () {
-    alert(getCookie("crmkey"));
-  });
-  
+  var key = $("#CRM-Key-input-field").val();
+  setCookie("crmkey", key, 10);
+});
+
+$("#CRM-Key-check-btn").click(function () {
+  alert(getCookie("crmkey"));
+});
+
+$("#CRM-Key-delete-btn").click(function () {
+  eraseCookie("crmkey");
+})
